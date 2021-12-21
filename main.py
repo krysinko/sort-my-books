310e563a3d1d76c2cca275d5bdfa060762f5342b
krysinko <magdalena.krys0@gmail.com>
2021-12-21 20:39:07 +0100

    main.py - cleanup

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 from mobi import Mobi
 import os, json, pprint, shutil
 
-# path = "C:/Users/Krysinko/Downloads/ebook/katalogi"
 sources = ["./test"]
 destination = "./test"
-# we shall store all the file names in this list
 
 fileList = {}
 mobi = '.mobi'
@@ -29,19 +27,11 @@ def createAuthorDir(author):
     if not os.path.exists(destination + author):
         os.mkdir(destination + author)
 
-
-#
-# def readBookData(p):
-#     return Mobi(f'{p}')
-
 def goThroughSource(s):
     for root, dirs, files in os.walk(s):
         # print(root, dirs, files)
-        # os.path.join(root,file)
         if len(files):
             for file in files:
-                # print(file)
-                # append the file name to the list
                 if file.find(mobi) > 0:
                     addExt(root, file, mobi)
                 elif file.find(epub) > 0:
@@ -53,19 +43,11 @@ def goThroughSource(s):
 
         # else: pass
 
-
-# print all the file names
-# for name in filelist:
-#     print(name)
-# print(fileList)
-
 for s in sources:
     goThroughSource(s)
 
 filesWithErrorDuringCopy = []
 for x in fileList:
-    # print(x, fileList[x])
-    # print(fileList.get(x), (fileList.get(x)).get(mobi) is not None)
     if fileList.get(x) and (fileList.get(x)).get(mobi) is not None:
         book = Mobi(f'{x + mobi}')
         book.parse();
@@ -81,17 +63,6 @@ for x in fileList:
 print(len(filesWithErrorDuringCopy), " files couldn't be copied. List:\n")
 for f in filesWithErrorDuringCopy:
     print(f)
-# path = os.path.dirname(__file__)
-# # book = Mobi(f'{path}/test/CharlesDarwin.mobi');
-# fileRelativePath = "/test/Gabi_i_Uwe_-_Uwe_von_Seltmann.mobi"
-# # fileRelativePath = "/test/Czapkins - Dominik Szczepanski.mobi"
-# book = Mobi(f"{path}{fileRelativePath}");
-# book.parse();
-#
-# pprint.pprint(book.config)
-# print( book.newFileName)
-
-
 
 
 
